22eb474b237d34b42ad7016db7b3f054034fb39c
Hans <lgs840522@gmail.com>
2024-05-18 15:33:16 +0800

    feat(all): 訂單功能 - 創建訂單（下）

--- src/main/java/com/example/springbootmall/service/OrderService.java
+++ src/main/java/com/example/springbootmall/service/OrderService.java
@@ -1,8 +1,11 @@
 package com.example.springbootmall.service;
 
+import com.example.springbootmall.model.Order;
 import com.example.springbootmall.dto.CreateOrderRequest;
 
 public interface OrderService {
+
+    Order getOrderById(Integer orderId);
 
     Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest);
 }
